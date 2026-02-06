4e4509d5b0a27bf355a26ea53eb56f020ca0395d
anishloop7 <anishrajjj@gmail.com>
2026-02-07 00:34:41 +0530

    using distinct

---
+++ src/StreamAPI/DistinctElements.java
@@ -0,0 +1,18 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DistinctElements {
+    public static void main(String[] args) {
+
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 6, 1, 2, 2, 3, 47);
+
+        List<Integer> n = num.stream().distinct().collect(Collectors.toList());
+
+        System.out.println(n);
+
+
+    }
+}
