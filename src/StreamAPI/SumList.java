1cd606ddedcdb37ece866391259cd5966fce1b18
anishloop7 <anishrajjj@gmail.com>
2026-02-07 00:03:28 +0530

    Using mapToInt for sum

---
+++ src/StreamAPI/SumList.java
@@ -0,0 +1,15 @@
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SumList {
+    public static void main(String[] args) {
+
+        List<Integer> numSum = Arrays.asList(1,2,3,4,7);
+        int sum = numSum.stream().mapToInt(Integer::intValue).sum();
+
+        System.out.println(sum);
+
+    }
+}
