463d77b812e4120dd4c8336ed5488ba43fa64bdd
anishloop7 <anishrajjj@gmail.com>
2026-02-08 16:35:03 +0530

    sorting testing

---
+++ src/codeLearning/TestingSort.java
@@ -0,0 +1,24 @@
+package codeLearning;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class TestingSort {
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {12,14,41,1,2,74};
+
+
+
+        //Arrays.sort(arr);
+
+
+        for(int i : arr){
+            System.out.println(i);
+        }
+       // Collections.sort(Arrays.arr);
+    }
+}
