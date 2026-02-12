43ad6afaf6bda08047387b8188acc84af29fbb81
anishloop7 <anishrajjj@gmail.com>
2026-02-13 03:13:42 +0530

    prime number

---
+++ src/codeLearning/PrimeNum.java
@@ -0,0 +1,21 @@
+package codeLearning;
+
+public class PrimeNum {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(45));
+
+    }
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        if (num <= 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+
+
+}
