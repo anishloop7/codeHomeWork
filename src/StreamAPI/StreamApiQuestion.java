6afdd4ae46a2ed02136ed90bd41ba49c97a548cc
anishloop7 <anishrajjj@gmail.com>
2026-02-21 08:55:00 +0530

    sorting

---
+++ src/StreamAPI/StreamApiQuestion.java
@@ -0,0 +1,20 @@
+package StreamAPI;
+
+import java.util.Arrays;
+
+public class StreamApiQuestion {
+
+    public static void main(String[] args) {
+        String str = "Alice Is testing application now";
+        int[] nums = {5, 2, 8, 1, 9};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums)); // [1, 2, 5, 8, 9]
+
+        System.out.println("--------------------");
+
+        Integer[] res =  {5, 2, 8, 1, 9};
+        Arrays.sort(res, (a,b)-> b-a);
+        System.out.println(Arrays.toString(res));
+
+    }
+}
