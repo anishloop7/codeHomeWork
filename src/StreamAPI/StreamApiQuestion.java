08d90990b30b61237b2b15df8beb864f92dfd42a
anishloop7 <anishrajjj@gmail.com>
2026-02-21 09:45:41 +0530

    sorting

--- src/StreamAPI/StreamApiQuestion.java
+++ src/StreamAPI/StreamApiQuestion.java
@@ -1,39 +1,43 @@
 package StreamAPI;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class StreamApiQuestion {
 
     public static void main(String[] args) {
-        String str = "Alice Is testing application now";
-        int[] nums = {5, 2, 8, 1, 9};
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums)); // [1, 2, 5, 8, 9]
-
-        System.out.println("--------------------");
-
-        Integer[] res =  {5, 2, 8, 1, 9};
-        Arrays.sort(res, (a,b)-> b-a);
-        System.out.println(Arrays.toString(res));
-
-        System.out.println("--------------------");
-
-        String[] fruits = {"Bananayyy77", "Apple","apple", "Cherry", "Mango"};
-//        Arrays.sort(fruits);
-//        System.out.println(Arrays.toString(fruits)); // [Apple, Banana, Cherry, Mango]
+//        String str = "Alice Is testing application now";
+//        int[] nums = {5, 2, 8, 1, 9};
+//        Arrays.sort(nums);
+//        System.out.println(Arrays.toString(nums)); // [1, 2, 5, 8, 9]
 //
-//        Arrays.sort(fruits, (a,b)->(a.length() - b.length()));
+//        System.out.println("--------------------");
+//
+//        Integer[] res =  {5, 2, 8, 1, 9};
+//        Arrays.sort(res, (a,b)-> b-a);
+//        System.out.println(Arrays.toString(res));
+//
+//        System.out.println("--------------------");
+//
+//        String[] fruits = {"Bananayyy77", "Apple","apple", "Cherry", "Mango"};
+////        Arrays.sort(fruits);
+////        System.out.println(Arrays.toString(fruits)); // [Apple, Banana, Cherry, Mango]
+////
+////        Arrays.sort(fruits, (a,b)->(a.length() - b.length()));
+////        System.out.println(Arrays.toString(fruits));
+//
+//        Arrays.sort(fruits, (a,b)-> {
+//            if(a.length()!=b.length()) return a.length() - b.length();
+//            else return a.compareTo(b);
+//        });
 //        System.out.println(Arrays.toString(fruits));
 
-        Arrays.sort(fruits, (a,b)-> {
-            if(a.length()!=b.length()) return a.length() - b.length();
-            else return a.compareTo(b);
-        });
-        System.out.println(Arrays.toString(fruits));
 
-
-
-
+       // List<Integer> nums = new ArrayList<>(List.of(3,2,3,34,45,555));
+        List<Integer> nums = Arrays.asList(5, 2, 8, 1, 9);
+        nums.sort(null);
+        System.out.println(nums);
 
     }
 }
