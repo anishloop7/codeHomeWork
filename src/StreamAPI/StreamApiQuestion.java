af9887ec3cb1aad51597dad9fe4386aade3b97d3
anishloop7 <anishrajjj@gmail.com>
2026-02-21 09:11:58 +0530

    sorting

--- src/StreamAPI/StreamApiQuestion.java
+++ src/StreamAPI/StreamApiQuestion.java
@@ -16,5 +16,24 @@ public class StreamApiQuestion {
         Arrays.sort(res, (a,b)-> b-a);
         System.out.println(Arrays.toString(res));
 
+        System.out.println("--------------------");
+
+        String[] fruits = {"Bananayyy77", "Apple","apple", "Cherry", "Mango"};
+//        Arrays.sort(fruits);
+//        System.out.println(Arrays.toString(fruits)); // [Apple, Banana, Cherry, Mango]
+//
+//        Arrays.sort(fruits, (a,b)->(a.length() - b.length()));
+//        System.out.println(Arrays.toString(fruits));
+
+        Arrays.sort(fruits, (a,b)-> {
+            if(a.length()!=b.length()) return a.length() - b.length();
+            else return a.compareTo(b);
+        });
+        System.out.println(Arrays.toString(fruits));
+
+
+
+
+
     }
 }
